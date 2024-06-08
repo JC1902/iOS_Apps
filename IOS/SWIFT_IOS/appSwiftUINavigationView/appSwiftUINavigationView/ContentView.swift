4c1d94a0c096b865c9b2ffeefe35be1c387c73c8
JC1902 <jorgecisneros2002@gmail.com>
2024-06-08 16:47:11 -0600

    lo ultimo parte 2

--- IOS/SWIFT_IOS/appSwiftUINavigationView/appSwiftUINavigationView/ContentView.swift
+++ IOS/SWIFT_IOS/appSwiftUINavigationView/appSwiftUINavigationView/ContentView.swift
@@ -7,11 +7,108 @@
 
 import SwiftUI
 
+struct emojiFrutas: Identifiable {
+    
+    let id = UUID()
+    let emoji: String
+    let nombre: String
+    let descripcion: String
+    
+}
+
+private let listaEmojis: [emojiFrutas] = [
+    emojiFrutas(
+        emoji: "🍊",
+        nombre: "Naranja",
+        descripcion: "La naranja es una fruta cítrica obtenida del naranjo dulce, del naranjo amargo y de naranjos de otras variedades o híbridos, de origen asiático."),
+    emojiFrutas(
+        emoji: "🍋",
+        nombre: "Limón",
+        descripcion: "El término limón es un nombre común de tres frutas cítricas fragantes y de sabor ácido, utilizadas principalmente en el sector alimentario. En primer lugar, se conoce como limones a aquellos frutos producidos por el híbrido Citrus × aurantifolia, árbol llamado popularmente limonero."),
+    emojiFrutas(
+        emoji: "🐉🍉",
+        nombre: "Fruta del dragón",
+        descripcion: "Pitires o fruta del dragón son nombres de los frutos de diversas especies del género Selenicereus de la familia Cactaceae, proveniente de América especialmente de Estados Unidos o México"),
+    emojiFrutas(
+        emoji: "⭐️🍉",
+        nombre: "Fruta de estrella",
+        descripcion: "La Averrhoa carambola es un arbusto tropical perenne, perteneciente a la familia oxalidaceae. Su fruto es la carambola, que también recibe los nombres de asterisa, fruta de estrella (por su apariencia a una estrella al observar su corte transversalmente)")
+]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            List(listaEmojis) {
+                
+                emojiFrutas in
+                NavigationLink(
+                    destination: detallesView(emojiFruta: emojiFrutas)) {
+                    
+                    HStack {
+                        
+                        emojiCirculoVista( emojiFruta: emojiFrutas )
+                        Text(emojiFrutas.nombre).font(.headline)
+                        
+                    }.padding(5)
+                    
+                } // NavigationLink
+                .navigationBarTitle("Frutas")
+                
+            } // List
+            
+        } // NavigationView
+    } // body
+}
+
+struct detallesView: View {
+    
+    let emojiFruta: emojiFrutas
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            
+            HStack {
+                
+                emojiCirculoVista( emojiFruta: emojiFruta ).padding()
+                Text(emojiFruta.nombre)
+                    .font(.title)
+                    .bold()
+                Spacer()
+                
+            }
+            
+            Text(emojiFruta.descripcion)
+                .padding(.top)
+            Spacer()
+            
+        }
+        .padding()
+        .navigationBarTitle(Text(emojiFruta.nombre), displayMode: .large)
+        
     }
+    
+}
+
+
+struct emojiCirculoVista: View {
+    
+    let emojiFruta: emojiFrutas
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Text(emojiFruta.emoji).shadow(radius: 5)
+                .font(.largeTitle)
+                .frame(width: 65, height: 65)
+                .overlay(Circle().stroke(Color.blue, lineWidth: 3.0))
+            
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- IOS/SWIFT_IOS/appTabBarViewController/appTabBarViewController/VCCurvasBezier.swift
+++ IOS/SWIFT_IOS/appTabBarViewController/appTabBarViewController/VCCurvasBezier.swift
@@ -9,10 +9,159 @@ import UIKit
 
 class VCCurvasBezier: UIViewController {
 
+    @IBOutlet weak var canvas: CurvasBezier!
+    
+    @IBOutlet weak var x1: UIStepper!
+    @IBOutlet weak var y1: UIStepper!
+    @IBOutlet weak var x2: UIStepper!
+    @IBOutlet weak var y2: UIStepper!
+    
+    @IBOutlet weak var puntoCx1: UIStepper!
+    @IBOutlet weak var puntoCy1: UIStepper!
+    @IBOutlet weak var puntoCx2: UIStepper!
+    @IBOutlet weak var puntoCy2: UIStepper!
+    
+    @IBOutlet weak var lblAncho: UILabel!
+    @IBOutlet weak var lblAlto: UILabel!
+    
+    @IBOutlet weak var btnAnchoAlto: UIButton!
+    
+    // Método para actualizar la curva de Bezier
+    func actualizarCurvaBezier() {
+        self.canvas.x1 = Int(x1.value)
+        self.canvas.y1 = Int(y1.value)
+        self.canvas.x2 = Int(x2.value)
+        self.canvas.y2 = Int(y2.value)
+        self.canvas.PCX1 = Int(puntoCx1.value)
+        self.canvas.PCY1 = Int(puntoCy1.value)
+        self.canvas.PCX2 = Int(puntoCx2.value)
+        self.canvas.PCY2 = Int(puntoCy2.value)
+        self.canvas.tipoCurva = .bezier
+        self.canvas.setNeedsDisplay()
+    }
+    
+    // Método para actualizar la curva de Bezier Cuadrática
+    func actualizarCurvaQuadBezier() {
+        self.canvas.x1 = Int(x1.value)
+        self.canvas.y1 = Int(y1.value)
+        self.canvas.x2 = Int(x2.value)
+        self.canvas.y2 = Int(y2.value)
+        self.canvas.PCX1 = Int(puntoCx1.value)
+        self.canvas.PCY1 = Int(puntoCy1.value)
+        self.canvas.tipoCurva = .bezierQuad
+        self.canvas.setNeedsDisplay()
+    }
+    
+    @IBAction func btnDibujarCurvaBezier(_ sender: UIButton) {
+        actualizarCurvaBezier()
+    }
+    
+    @IBAction func btnDibujarCurvaQuadBezier(_ sender: UIButton) {
+        actualizarCurvaQuadBezier()
+    }
+    
+    
+    @IBAction func btnAnchoAlto(_ sender: UIButton) {
+        
+        lblAlto.text! += " " + String( canvas.alto )
+        lblAncho.text! += " " + String ( canvas.ancho )
+        
+        btnAnchoAlto.isEnabled = false
+        
+    }
+    
+    @IBAction func x1(_ sender: UIStepper) {
+        x1.value = sender.value
+        
+        switch self.canvas.tipoCurva {
+        case .bezier:
+            actualizarCurvaBezier()
+        case .bezierQuad:
+            actualizarCurvaQuadBezier()
+        }
+        
+    }
+    
+    @IBAction func y1(_ sender: UIStepper) {
+        y1.value = sender.value
+        
+        switch self.canvas.tipoCurva {
+        case .bezier:
+            actualizarCurvaBezier()
+        case .bezierQuad:
+            actualizarCurvaQuadBezier()
+        }
+    }
+    
+    @IBAction func x2(_ sender: UIStepper) {
+        x2.value = sender.value
+        
+        switch self.canvas.tipoCurva {
+        case .bezier:
+            actualizarCurvaBezier()
+        case .bezierQuad:
+            actualizarCurvaQuadBezier()
+        }
+    }
+    
+    @IBAction func t2(_ sender: UIStepper) {
+        y2.value = sender.value
+        
+        switch self.canvas.tipoCurva {
+        case .bezier:
+            actualizarCurvaBezier()
+        case .bezierQuad:
+            actualizarCurvaQuadBezier()
+        }
+    }
+    
+    @IBAction func puntoCx1(_ sender: UIStepper) {
+        puntoCx1.value = sender.value
+        
+        switch self.canvas.tipoCurva {
+        case .bezier:
+            actualizarCurvaBezier()
+        case .bezierQuad:
+            actualizarCurvaQuadBezier()
+        }
+    }
+    
+    @IBAction func puntoCy1(_ sender: UIStepper) {
+        puntoCy1.value = sender.value
+        
+        switch self.canvas.tipoCurva {
+        case .bezier:
+            actualizarCurvaBezier()
+        case .bezierQuad:
+            actualizarCurvaQuadBezier()
+        }
+    }
+    
+    @IBAction func puntoCx2(_ sender: UIStepper) {
+        puntoCx2.value = sender.value
+        
+        switch self.canvas.tipoCurva {
+        case .bezier:
+            actualizarCurvaBezier()
+        case .bezierQuad:
+            actualizarCurvaQuadBezier()
+        }
+    }
+    
+    @IBAction func puntoCy2(_ sender: UIStepper) {
+        puntoCy2.value = sender.value
+        
+        switch self.canvas.tipoCurva {
+        case .bezier:
+            actualizarCurvaBezier()
+        case .bezierQuad:
+            actualizarCurvaQuadBezier()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
